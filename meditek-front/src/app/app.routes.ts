247e0b1e999fc33962924c830ef201e7f71b71ee
Cristopher Gabriel Vera Quispe <114591444+CristopherVQ1024@users.noreply.github.com>
2026-04-21 21:15:05 -0500

    Creación del dashboard medico

--- meditek-front/src/app/app.routes.ts
+++ meditek-front/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardMedicoComponent } from './dashboard-medico/dashboard-medico.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard-medico', component: DashboardMedicoComponent },
 ];
---
+++ meditek-front/src/app/dashboard-paciente/dashboard-paciente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-paciente',
+  imports: [],
+  templateUrl: './dashboard-paciente.component.html',
+  styleUrl: './dashboard-paciente.component.scss'
+})
+export class DashboardPacienteComponent {
+
+}
